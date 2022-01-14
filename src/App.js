7e1132e60b55460b0186bda600e8bdd2e2053a88
Jose Manuel Serrano Amaut <a20122128@pucp.pe>
2022-01-13 19:00:56 -0500

    [INIT]

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ElevateAppBar from "./components/ElevateAppBar";
+import { Fragment } from "react";
+function App() {
+  return (
+    <Fragment>
+      <div className="main-wrapper" style={{}}>
+          <ElevateAppBar /> 
+      </div>
+   
+      
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/ElevateAppBar.jsx
@@ -0,0 +1,87 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+//import Typography from "@mui/material/Typography";
+//import CssBaseline from "@mui/material/CssBaseline";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+//import Box from "@mui/material/Box";
+//import Container from "@mui/material/Container";
+import { Avatar } from "@mui/material";
+
+function ElevationScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+export default function ElevateAppBar(props) {
+  // const { children } = props;
+  return (
+    <React.Fragment>
+      {/* <CssBaseline /> */}
+      <ElevationScroll {...props}>
+        <AppBar
+          style={{
+            height: "64px",
+            backgroundColor: "rgba(255,255,255,0.82)",
+            color: "black",
+            backdropFilter: "blur(20px)",
+            boxShadow: "inset 0px -1px 1px #eaeef3",
+          }}
+        >
+          <Toolbar>
+            <Avatar
+              style={{
+                cursor: "pointer",
+              }}
+             /*  onClick={() => {
+                window.location.replace("http://www.dropzone-ui.com");
+              }} */
+              alt="mongo-drive"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIhPxc1jY5Z8np07vkjWtHlhKQVnWgnq3b3xqJJY5yBavbjZFOpf9OUYG1lHF06p6kNZ4&usqp=CAU"
+            />
+            <div
+              style={{
+                cursor: "pointer",
+                margin: "0 10px",
+                fontSize: "1.5rem",
+                lineHeight: "1.334",
+                letterSpacing: "0em",
+                fontWeight: "400",
+              }}
+              onClick={() => {
+                window.location.replace("http://www.dropzone-ui.com");
+              }}
+            >
+              {"Mongo Drive Files"}
+            </div>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <Toolbar />
+      {/*  <Container>
+        <Box sx={{ my: 2 }}>
+          {children ||
+            [...new Array(12)]
+              .map(
+                () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
+              )
+              .join("\n")}
+        </Box>
+      </Container> */}
+    </React.Fragment>
+  );
+}
